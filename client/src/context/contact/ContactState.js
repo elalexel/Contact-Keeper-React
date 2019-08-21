58a9db08a9ff183ccb0380eddfb4f295ed42f7b9
alexpargon <alexpargon@gmail.com>
2019-08-21 18:34:43 +0200

    added set and clear current contact state in context

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,7 +37,8 @@ const ContactState = props => {
         phone: "333-333-3333",
         type: "professional"
       }
-    ]
+    ],
+    current: null
   };
 
   const [state, dispatch] = useReducer(ContactReducer, initialState);
@@ -59,9 +60,20 @@ const ContactState = props => {
     });
   };
 
-  // Set CUrrent Contact
+  // Set Current Contact
+  const setCurrent = contact => {
+    dispatch({
+      type: SET_CURRENT,
+      payload: contact
+    });
+  };
 
   // Clear Current Contact
+  const clearCurrent = () => {
+    dispatch({
+      type: CLEAR_CURRENT
+    });
+  };
 
   // Update Contact
 
@@ -71,7 +83,14 @@ const ContactState = props => {
 
   return (
     <ContactContext.Provider
-      value={{ contacts: state.contacts, addContact, deleteContact }}
+      value={{
+        contacts: state.contacts,
+        current: state.current,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent
+      }}
     >
       {props.children}
     </ContactContext.Provider>

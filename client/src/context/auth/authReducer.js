25ab66decf247ab6166c213732ea5cfc6100ee23
alexpargon <alexpargon@gmail.com>
2019-08-31 13:38:42 +0200

    linked register with authentication

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -12,6 +12,13 @@ import { truncate } from "fs";
 
 export default (state, action) => {
   switch (action.type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload
+      };
     case REGISTER_SUCCESS:
       localStorage.setItem("token", action.payload.token);
       return {
@@ -21,6 +28,7 @@ export default (state, action) => {
         loading: truncate
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem("token");
       return {
         ...state,
